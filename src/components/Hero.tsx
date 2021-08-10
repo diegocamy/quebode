aa334287ae6a41f3016dd504103dc02398ce5a19
Diego <diegocamyy@gmailcom>
2021-08-10 16:51:29 -0300

    Added film genres on a separate file and started creating a select element to quick search by genre

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,11 @@
-import { Flex, Box, useMediaQuery } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  useMediaQuery,
+  Heading,
+  Text,
+  Select,
+} from "@chakra-ui/react";
 import movies from "../assets/movies.jpg";
 
 function Hero() {
@@ -13,6 +20,7 @@ function Hero() {
       backgroundPosition="center"
       backgroundSize="cover"
       position="relative"
+      textAlign="center"
     >
       <Box
         position="absolute"
@@ -20,9 +28,27 @@ function Hero() {
         left="0"
         w="100%"
         h="100%"
-        zIndex="1"
-        bgGradient="linear(to-b, rgba(0,0,0,0.3), background)"
+        bgGradient="linear(to-b, rgba(0,0,0,0.6), background)"
       />
+      <Heading zIndex="2" fontSize={isMobile ? "3xl" : "6xl"}>
+        Buscando algo para ver?
+      </Heading>
+      <Text zIndex="2" fontSize={isMobile ? "md" : "xl"}>
+        Encontrá peliculas para mirar YAAAAA
+      </Text>
+      <Select
+        placeholder="Búsqueda rápida por género"
+        maxWidth="450px"
+        textAlign="center"
+        mt="5"
+        bgColor="white"
+        color="black"
+        borderRadius="3xl"
+        border="3px solid red"
+        mx="2"
+      >
+        {/* TODO: options with film genres */}
+      </Select>
     </Flex>
   );
 }
