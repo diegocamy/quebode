16c01a6192741c4e872114d79b61608872cdd815
dcamy <diegocamyy@gmail.com>
2021-08-18 23:05:42 -0300

    Added scroll to top component

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import MovieDetails from "./pages/MovieDetails";
 
 function Routes() {
   return (
     <Router>
+      <ScrollToTop />
       <NavBar />
       <Switch>
         <Route path="/" exact component={Home} />
